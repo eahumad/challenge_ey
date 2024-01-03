d158eb9c877ab9f47e2e9627434163f458b05be1
Emilio Ahumada <m.donoso.mun@gmail.com>
2024-01-03 16:48:18 -0300

    feat(test): prueba unitaria para creación de usuario

---
+++ src/test/java/com/ey/challenge/controller/UserControllerTest.java
@@ -0,0 +1,48 @@
+package com.ey.challenge.controller;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void CreateUserTest() throws Exception {
+        String user1 = "{\"name\": \"Esteban\",\"email\": \"32333@ban.cl\",\"password\": \"sdasd22A\",\"phones\": [{\"number\": \"1234567\",\"citycode\": \"1\",\"countrycode\": \"57\"},{\"number\": \"323das\",\"citycode\": \"43\",\"countrycode\": \"2324\"}]}";
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/api/users")
+                .content(user1)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+
+        MockHttpServletResponse response = result.getResponse();
+
+        assertEquals(HttpStatus.CREATED.value(), response.getStatus());
+
+    }
+
+}
